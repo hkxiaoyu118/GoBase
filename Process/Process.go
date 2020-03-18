b48a96ac159af2a97223198c431bf2472a702eba
hkxiaoyu118 <hkxiaoyu118@gmail.com>
2020-03-18 21:53:53 +0800

    添加了两个有用的常用函数

---
+++ Process/Process.go
@@ -0,0 +1,31 @@
+package Process
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"os/exec"
+	"time"
+)
+
+var Timeout = 240 * time.Second //超时时间
+
+//以超时的方式执行命令行
+func CommandWithTimeout(cmdName string, arg string, stdinData string) ([]byte, error) {
+	stdin := bytes.NewBuffer([]byte(stdinData))
+	ctxt, cancel := context.WithTimeout(context.Background(), Timeout)
+	defer cancel()
+	cmd := exec.CommandContext(ctxt, cmdName, arg)
+	cmd.Stdin = stdin
+
+	if out, err := cmd.Output(); err != nil {
+		//检测报错是否由于超时引起
+		if ctxt.Err() != nil && ctxt.Err() == context.DeadlineExceeded {
+			return nil, errors.New("command timeout")
+		}
+		//报错不是由于超时引起
+		return out, err
+	} else {
+		return out, nil
+	}
+}
